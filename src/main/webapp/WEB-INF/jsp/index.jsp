7af7b9881c9c0760c63a0db774f0fc9f1af0f724
s4b3l0 <sabelo.nkosi@rocketmail.com>
2023-11-14 21:34:26 +0200

    Refresh

---
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<html>
+<head>
+    <title>View Items</title>
+</head>
+<body>
+<p>Hello</p>
+</body>
+
+</html>
---
+++ src/test/java/com/example/finanancialdashboard/FinanancialDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finanancialdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanancialDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
