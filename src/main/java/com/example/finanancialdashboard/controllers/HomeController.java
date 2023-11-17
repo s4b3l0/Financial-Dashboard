37eb3feb7cb35f6c55896ae063372f0c212df51a
Sabelo-Nkosi <sabelo@harambee.co.za>
2023-11-17 12:03:46 +0200

    refactoring controller and adding mock

--- src/main/java/com/example/finanancialdashboard/controllers/HomeController.java
+++ src/main/java/com/example/finanancialdashboard/controllers/HomeController.java
@@ -1,7 +1,6 @@
 package com.example.finanancialdashboard.controllers;
 
 import com.example.finanancialdashboard.config.Routes;
-import jakarta.websocket.server.PathParam;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
